f326dfe74143968da33ec7472cc974ec9fbfeaa4
madhu-b <madhu@liftoffllc.com>
2015-04-17 12:43:29 +0530

    Initial Commit

---
+++ gitEx/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitEx
+//
+//  Created by anil on 17/04/15.
+//  Copyright (c) 2015 Madhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
